9388ddf884758fb68ca1f13517f41958fc9c9455
Ethan Grab <ethangrab@gmail.com>
2019-04-30 13:56:48 -0600

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,52 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      # resources :items, only: [:show, :index]
+      get '/items.json', to: 'items#index'
+      get '/items/:id.json', to: 'items#show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      # resources :invoices, only: [:show, :index]
+      get '/invoices.json', to: 'invoices#index'
+      get '/invoices/:id.json', to: 'invoices#show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get '/transactions.json', to: 'transactions#index'
+      get '/transactions/:id.json', to: 'transactions#show'
+      # resources :transactions, only: [:show, :index]
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      # resources :merchants, only: [:show, :index]
+      get '/merchants.json', to: 'merchants#index'
+      get '/merchants/:id.json', to: 'merchants#show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      # resources :customers, only: [:show, :index]
+      get '/customers.json', to: 'customers#index'
+      get '/customers/:id.json', to: 'customers#show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      # resources :customers, only: [:show, :index]
+      get '/invoiceitems.json', to: 'invoice_items#index'
+      get '/invoiceitems/:id.json', to: 'invoice_items#show'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
